49edaca4b221816fce2557ba98ff51a818b6364c
ccenjie <289842316@qq.com>
2018-09-01 18:01:10 +0800

    ，通过就完成

--- src/main/java/cn/ccenjie/activemq/wb/BaseWebSocket.java
+++ src/main/java/cn/ccenjie/activemq/wb/BaseWebSocket.java
@@ -5,14 +5,27 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+
 /**
  * @author cenjunjie
  * 2018/8/31
  */
 public class BaseWebSocket extends TextWebSocketHandler {
 
+    private static final String FILE_PATH = "D:/132.PNG";
+
     @Override
     protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+        ByteBuffer payload = message.getPayload().asReadOnlyBuffer();
+        byte[] array = payload.array();
+        try {
+            FileOutputStream out = new FileOutputStream(FILE_PATH);
+            out.write(array);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
         super.handleBinaryMessage(session, message);
     }
 
